a96a19c5817960cac08cd7cbc64916a33d05be51
Illia Hrymalo <grimaloilya@gmail.com>
2024-12-18 21:33:58 -0700

    all tasks completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,46 @@ import ForbesList from './components/ForbesList/ForbesList';
 import Heading from './components/Heading/Heading';
 import Section from './components/Section/Section';
 import Statistics from './components/Statistics/Statistics';
+import article from './data/article.json';
+import stats from './data/stats.json';
+import forbes from './data/forbes.json';
+import transactions from './data/transactions.json';
+import { formatDateToNow } from './helpers/date';
 
 export const App = () => {
+  const { poster, tag, title, description, name, avatar, postedAt } = article;
+
+  // let arr = [1, 2, 3, 4, 5];
+  // for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+  //   arr[i] = arr[i] + arr[arr.length - 1 - i];
+  //   arr[arr.length - 1 - i] = arr[i] - arr[arr.length - 1 - i]
+  //   arr[i] = arr[i] - arr[arr.length - 1 - i]
+  //   console.log(arr);
+  // }
+
   return (
     <Section>
       <Container>
         <Heading title="Task 1 Blog Card" bottom />
-        <BlogCard />
+        <BlogCard
+          poster={poster}
+          tag={tag}
+          title={title}
+          description={description}
+          name={name}
+          avatar={avatar}
+          postedAt={() => formatDateToNow(postedAt)}
+        />
 
         <Heading title="Task 2 Statistics" top bottom />
-        <Statistics title="Main Statistics" />
+        <Statistics title="Main Statistics" stats={stats} />
+        <Statistics stats={stats} />
 
         <Heading title="Task 3 Forbes list" top bottom />
-        <ForbesList />
+        <ForbesList list={forbes} />
 
         <Heading title="Task 4 Crypto history" top bottom />
-        <CryptoHistory />
+        <CryptoHistory items={transactions} />
       </Container>
     </Section>
   );
